60bb664530a68714aef9e0098bf122e65931d81a
Mohamed Hamada <moha2016226@gmail.com>
2024-10-14 04:32:49 -0700

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace TrainingApp.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            while (true)
+            {
+                Console.Write(">> ");
+                var input = Console.ReadLine().Trim();
+                var whiteSpaceIndex = input.IndexOf(' ');
+                var command = input.Substring(0, whiteSpaceIndex).ToLower();
+                var path = input.Substring(whiteSpaceIndex + 1).Trim();
+                //Console.WriteLine(command);
+                //Console.Write(path);
+                if (command == "list")
+                {
+                    foreach (var entry in Directory.GetDirectories(path))
+                        Console.WriteLine($"[dir]\t {entry}");
+                    foreach (var entry in Directory.GetFiles(path))
+                        Console.WriteLine($"[file]\t {entry}");
+                }
+                else if (command == "info")
+                {
+                    if (Directory.Exists(path))
+                    {
+                        var dirInfo = new DirectoryInfo(path); ;
+                        Console.WriteLine("The type is dir");
+                        Console.WriteLine($"the dir created At: {dirInfo.CreationTime}");
+                        Console.WriteLine($"The dir modified At: {dirInfo.LastWriteTime}");
+                    }
+                    else if (File.Exists(path))
+                    {
+                        var fileInfo = new FileInfo(path);
+                        Console.WriteLine("The Type : File");
+                        Console.WriteLine($"THe time created At : {fileInfo.CreationTime}");
+                        Console.WriteLine($"the last modified time at: {fileInfo.LastWriteTime}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("the path does't exists!");
+                    }
+
+
+
+                }
+                else if (command == "mkdir")
+                {
+                    Directory.CreateDirectory(path);
+
+                }
+                else if (command == "remove")
+                {
+                    Directory.Delete(path);
+
+                }
+                else if (command == "print")
+                {
+                    if (File.Exists(path))
+                    {
+                        var content = File.ReadAllText(path);
+                        Console.WriteLine(content);
+                    }
+                }
+                else if (command == "exit")
+                {
+                    break;
+                }
+            }
+        }
+
+    }
+}
